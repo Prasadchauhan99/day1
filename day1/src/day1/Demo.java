029936adf941c3a3325a3b9b0b4b123ed7eed2a6
prasad.chauhan.k <prasad.chauhan.k@HDC2LF2C8TG7.dir.svc.accenture.com>
2022-07-29 10:52:16 +0530

    First commit

---
+++ day1/src/day1/Demo.java
@@ -0,0 +1,47 @@
+package day1;
+
+interface Parser 
+{
+	String parse(String str);
+	
+}
+class StringParser 
+{
+	public static String convert(String s)
+	{
+		if(s.length()<=3)
+			s = s.toUpperCase();
+		
+		else 
+			s = s.toLowerCase();
+		return s;
+	}
+}
+
+
+
+class MyPrinter
+{
+	public void print(String str,Parser p)
+	{
+		str = p.parse(str);
+		System.out.println(str);
+	}
+}
+public class Demo {
+	
+	public static void main(String args[])
+	{
+//		List<String> names = Arrays.asList("Chauhan","prasad","anthony");
+		
+//		
+		
+		
+				
+
+		String str = "Chauhan";
+		MyPrinter mp = new MyPrinter();
+		mp.print(str,(StringParser::convert));
+	}
+
+}
